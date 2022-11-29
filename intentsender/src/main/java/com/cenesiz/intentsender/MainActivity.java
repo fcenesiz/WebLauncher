115b5af7eeaadde22b1659b52e09a60d6fa9efc3
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2022-11-29 15:28:41 +0300

    intentsender created

--- intentsender/src/main/java/com/cenesiz/intentsender/MainActivity.java
+++ intentsender/src/main/java/com/cenesiz/intentsender/MainActivity.java
@@ -1,5 +1,9 @@
 package com.cenesiz.intentsender;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonOpenWebpage = findViewById(R.id.buttonOpenWebpage);
+
+        buttonOpenWebpage.setOnClickListener(view -> {
+            Intent intent = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/fcenesiz")
+                    );
+            startActivity(intent);
+        });
     }
+
 }
