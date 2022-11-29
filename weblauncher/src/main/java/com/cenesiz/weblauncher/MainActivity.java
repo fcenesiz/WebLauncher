ef88cfb4e75c4be0d83d043ba029bd7be003b26e
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2022-11-29 15:35:30 +0300

    weblauncher created

--- weblauncher/src/main/java/com/cenesiz/weblauncher/MainActivity.java
+++ weblauncher/src/main/java/com/cenesiz/weblauncher/MainActivity.java
@@ -1,7 +1,15 @@
 package com.cenesiz.weblauncher;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent receivedIntent = getIntent();
+        Uri uri = receivedIntent.getData();
+        URL webPageUrl = null;
+
+        if (uri != null){
+            try {
+                webPageUrl = new URL(
+                        uri.getScheme(),
+                        uri.getHost(),
+                        uri.getPath()
+                );
+            } catch (MalformedURLException e) {
+                Toast.makeText(
+                        this,
+                        "Intent exception occured",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        }
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(webPageUrl.toString());
     }
 }
